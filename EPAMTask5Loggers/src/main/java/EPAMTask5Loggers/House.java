0bb5be03fc880d70e3a055e516ccb717b631280b
jaiswal547 <jaiswaladitya547@gmail.com>
2020-03-01 20:08:45 +0530

    Committing my task

---
+++ EPAMTask5Loggers/src/main/java/EPAMTask5Loggers/House.java
@@ -0,0 +1,16 @@
+package EPAMTask5Loggers;
+import javax.swing.*;
+
+import com.sun.istack.internal.logging.Logger;
+import com.sun.org.apache.bcel.internal.generic.LOR;
+
+import java.awt.event.*;
+
+public class House{
+	private static Logger LOGGER=Logger.getLogger(Main.class);
+	public static void main(String args[]) {
+		
+		new Construction();
+	}
+
+	}
